696a889db93a103949f61033d8ed27601f499af3
JANGYONGSEONG <yongsung94@naver.com>
2019-05-14 12:26:32 +0000

    test: Add plans api PATCH title, PATCH content, DELETE

--- src/server/api/plans/plans.spec.js
+++ src/server/api/plans/plans.spec.js
@@ -22,7 +22,6 @@ describe('GET /plans', () => {
     });
 });
 
-
 describe('POST /plans', () => {
     it('should respond with plan', (done) => {
       request(app)
@@ -40,6 +39,54 @@ describe('POST /plans', () => {
           res.body.should.have.properties('title','content');
           res.body.title.should.equal('Develope TODOList');
           res.body.content.should.equal('Complete 2019 summer coding');
+        });
+        done();
+    });
+});
+
+describe('PATCH /plans/:id/:title', () => {
+  it('should respond with plan title', (done) => {
+    request(app)
+      .patch('/plans/Develope TODOList_1')
+      .expect(200)
+      .end((err, res)=>{
+        if(err){
+          done(err);
+          return;
+        }
+        res.body.should.have.properties('title');
+        res.body.title.should.equal('Develope TODOList_1');
+      });
+      done();
+  });
+});
+
+describe('PATCH /plans/:id/:content', () => {
+  it('should respond with plan content', (done) => {
+    request(app)
+      .patch('/plans/Complete 2019 Summer Coding')
+      .expect(200)
+      .end((err, res)=>{
+        if(err){
+          done(err);
+          return;
+        }
+        res.body.should.have.properties('title');
+        res.body.title.should.equal('Complete 2019 Summer Coding');
+      })
+  });
+});
+
+describe('DELETE /plans/:id', () => {
+    it('should return 204 status code', (done) => {
+      request(app)
+        .delete('/plans/1')
+        .expect(204)
+        .end((err, res)=>{
+          if(err){
+            done(err);
+            return;
+          }
         });
         done();
     });
