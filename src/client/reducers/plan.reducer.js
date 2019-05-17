5c1998207cd47f7b2becb61e9bd9966f47dd6663
JANGYONGSEONG <yongsung94@naver.com>
2019-05-17 05:11:18 +0000

    feat: Add initialState property

--- src/client/reducers/plan.reducer.js
+++ src/client/reducers/plan.reducer.js
@@ -1,91 +1,115 @@
 import {PlanConstants} from '../constants';
 
-const initialState = [];
+const initialState = {
+  isCreatePlanOn:false,
+  plans:[]
+};
 
 const plan = (state = initialState, action) => {
 
+  var updatedPlans;
+
   switch (action.type){
+    case PlanConstants.PLAN_FORM_ON:
+      return Object.assign({},state,{
+        isCreatePlanOn:true
+      });
+
+    case PlanConstants.PLAN_FORM_OFF:
+      return Object.assign({},state,{
+        isCreatePlanOn:false
+      });
+
     case PlanConstants.RECEIVE_PLAN_SUCCESS:
-      return Object.assign({},{ action.plans;});
+      return Object.assign({},state,{ plans:action.plans});
 
     case PlanConstants.RECEIVE_PLAN_FAILURE:
       return state;
 
     case PlanConstants.CREATE_PLAN_SUCCESS:
       return Object.assign({}, state, {
-        action.plan;
+        plans:[...state.plans,action.plan]
       });
 
     case PlanConstants.CREATE_PLAN_FAILURE:
       return state;
 
     case PlanConstants.REMOVE_PLAN_SUCCESS:
-      const updatedPlans = state.filter(plan => {
-        return plan.id != action.id;
+      return Object.assign({},state,{
+        plans: state.plans.filter(plan => {
+          return plan.id != action.id;
+        })
       });
-      return updatedPlans;
 
     case PlanConstants.REMOVE_PLAN_FAILURE:
       return state;
 
     case PlanConstants.CHANGE_TITLE_SUCCESS:
-    const updatedPlans = state.map(plan => {
-      if(plan.id === action.plan.id){
-        return { ...plan, ...action.plan.title };
-      }
-      return plan;
-    });
-    return updatedPlans;
+      return Object.assign({},state,{
+        plans: state.plans.map(plan => {
+          if(plan.id === action.plan.id){
+            return Object.assign({},plan,{title:action.plan.title});
+          }
+          return plan;
+        })
+      });
 
     case PlanConstants.CHANGE_TITLE_FAILURE:
       return state;
 
     case PlanConstants.CHANGE_CONTENT_SUCCESS:
-    const updatedPlans = state.map(plan => {
-      if(plan.id === action.plan.id){
-        return { ...plan, ...action.plan.content };
-      }
-      return plan;
-    });
-    return updatedPlans;
+      return Object.assign({},state,{
+        plans: state.plans.map(plan => {
+          if(plan.id === action.plan.id){
+            return Object.assign({},plan,{content:action.plan.content});
+          }
+          return plan;
+        })
+      });
 
     case PlanConstants.CHANGE_CONTENT_FAILURE:
       return state;
 
     case PlanConstants.CHANGE_DATE_SUCCESS:
-    const updatedPlans = state.map(plan => {
-      if(plan.id === action.plan.id){
-        return { ...plan, ...action.plan.date };
-      }
-      return plan;
-    });
-    return updatedPlans;
+      return Object.assign({},state,{
+        plans: state.plans.map(plan => {
+          if(plan.id === action.plan.id){
+            return Object.assign({},plan,{date:action.plan.date});
+          }
+          return plan;
+        })
+      });
 
     case PlanConstants.CHANGE_DATE_FAILURE:
       return state;
 
     case PlanConstants.CHANGE_PRIORITY_SUCCESS:
-      const updatedPlans = state.map(plan => {
-        if(plan.id === action.plan.id){
-          return { ...plan, ...action.plan.priority };
-        }
-        return plan;
+      return Object.assign({},state,{
+        plans: state.plans.map(plan => {
+          if(plan.id === action.plan.id){
+            return Object.assign({},plan,{priority:action.plan.priority});
+          }
+          return plan;
+        })
       });
-      return updatedPlans;
 
     case PlanConstants.CHANGE_PRIORITY_FAILURE:
       return state;
 
     case PlanConstants.CHANGE_STATUS_SUCCESS:
-      const updatedPlans = state.map(plan => {
-        if(plan.id === action.plan.id){
-          return { ...plan, ...action.plan.status };
-        }
-        return plan;
+      return Object.assign({},state,{
+        plans: state.plans.map(plan => {
+          if(plan.id === action.plan.id){
+            return Object.assign({},plan,{status:action.plan.status});
+          }
+          return plan;
+        })
       });
-      return updatedPlans;
 
     case PlanConstants.CHANGE_STATUS_FAILURE:
+      return state;
+
+    default:
       return state;
   }
 }
